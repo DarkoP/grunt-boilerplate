4e3cdb7aa6a1e0d7c10c5b60262e5dad00580745
DarkoP <darko.pervan@gmail.com>
2017-06-05 22:23:21 +0200

    add dev and production env to Grundfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,13 +1,11 @@
 module.exports = function(grunt) {
 
-  // ===========================================================================
   // CONFIGURE GRUNT ===========================================================
-  // ===========================================================================
   grunt.initConfig({
 
     pkg: grunt.file.readJSON('package.json'),
 
-    // configure jshint to validate js files ===================================
+    // configure jshint to validate js files -----------------------------------
     jshint: {
       options: {
         reporter: require('jshint-stylish')
@@ -15,7 +13,7 @@ module.exports = function(grunt) {
       all: ['Grunfile.js', 'src/**/*.js']
     },
 
-    // configure uglify to minify js files =====================================
+    // configure uglify to minify js files -------------------------------------
     uglify: {
       options: {
         banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
@@ -23,11 +21,20 @@ module.exports = function(grunt) {
       build: {
         files: {
           'dist/js/script.min.js': 'src/js/script.js'
+          // 'dist/js/script.min.js': ['src/js/script.js', 'src/js/script2.js']   // min multiple files
         }
       }
+      
+      // Different Tasks for Different Environments
+      // ,dev: { 
+      //   files: { 'dist/js/script.min.js': ['src/js/script.js', 'src/js/script2.js'] } 
+      // }, 
+      // production: { 
+      //   files: { 'dist/js/script.min.js': 'src/**/*.js' } 
+      // } 
     },
-
-    // compile less stylesheets to css =========================================
+    
+    // compile less stylesheets to css -----------------------------------------
     less: {
       build: {
         files: {
@@ -36,7 +43,7 @@ module.exports = function(grunt) {
       }
     },
 
-    // configure cssmin to minify css files ====================================
+    // configure cssmin to minify css files ------------------------------------
     cssmin: {
       options: {
         banner: '/*\n <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> \n*/\n'
@@ -48,7 +55,7 @@ module.exports = function(grunt) {
       }
     },
 
-    // configure watch to auto update ==========================================
+    // configure watch to auto update ------------------------------------------
     watch: {
       stylesheets: {
         files: ['src/**/*.css', 'src/**/*.less'],
@@ -59,21 +66,28 @@ module.exports = function(grunt) {
         tasks: ['jshint', 'uglify']
       }
     }
-
   });
 
-  // ===========================================================================
   // LOAD GRUNT PLUGINS ========================================================
-  // ===========================================================================
-  grunt.loadNpmTasks('grunt-contrib-jshint');
-  grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-less');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
-  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.loadNpmTasks('grunt-contrib-jshint');	      // Validate files using jshint
+  grunt.loadNpmTasks('grunt-contrib-uglify');	      // Minify JS files using UglifyJS
+  grunt.loadNpmTasks('grunt-contrib-less');	        // Compile LESS files to CSS
+  grunt.loadNpmTasks('grunt-contrib-cssmin');	      // Compress CSS files
+  grunt.loadNpmTasks('grunt-contrib-watch');	      // Run tasks whenever watched files are changed
+  
+  // grunt.loadNpmTasks('grunt-contrib-clean');	      // Clean up files and folders
+  // grunt.loadNpmTasks('grunt-contrib-copy');	      // Copy files and folders
+  // grunt.loadNpmTasks('grunt-contrib-concat');	    // Combine files into a single file
+  // grunt.loadNpmTasks('grunt-contrib-imagemin');	  // Minify PNG, JPG, and GIFs
+  // grunt.loadNpmTasks('grunt-contrib-compass');	    // Compile SASS to CSS using Compass
+  // grunt.loadNpmTasks('grunt-contrib-htmlmin');	    // Minify HTML files
 
-  // ===========================================================================
-  // CREATE TASKS ==============================================================
-  // ===========================================================================
+  // CREATE TASKS ==============================================================  
+  // this default task will go through all configuration (dev and production) in each task 
   grunt.registerTask('default', ['jshint', 'uglify', 'cssmin', 'less']);
 
+  // // this task will only run the dev configuration 
+  // grunt.registerTask('dev', ['jshint:dev', 'uglify:dev', 'cssmin:dev', 'less:dev']);
+  // // only run production configuration 
+  // grunt.registerTask('production', ['jshint:production', 'uglify:production', 'cssmin:production', 'less:production']);
 };
